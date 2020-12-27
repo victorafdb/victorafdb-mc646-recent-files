e3a82ada7108dcb498c32f832ac2a11ba879f3d1
Victor Fernandes <65911587+victorafdb@users.noreply.github.com>
2020-12-27 18:20:22 -0300

    Initial commit

---
+++ src/recent_files.py
@@ -0,0 +1,32 @@
+# Recent Items .py
+#
+# Need to implement a RecentFiles class that maintains an a queue with
+# the capability of:
+# 1- If a file is opened and it is already inserted in the queue, it becomes the
+# top file
+# 2- The oldest item is removed when the queue is full and a new file is insert
+# ed
+# 3- Flush itself
+# 4- Block the addition of new files
+
+class File:
+    def __init__(self, path, content):
+        self.path = path
+        self.content = content
+        self.next = None
+
+
+class RecentFiles:
+    def __init__(self, capacity):
+        self.current_load = 0
+        self.capacity = capacity
+
+    def is_full(self):
+        pass
+
+def main():
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
