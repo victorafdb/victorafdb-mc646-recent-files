de529db0c65d5d2a5a0961cb42eb8a5faf9716e0
Victor Fernandes <65911587+victorafdb@users.noreply.github.com>
2020-12-27 23:20:19 -0300

    refactor [Added methods to support insertions and removal in RecentFiles]

--- src/recent_files.py
+++ src/recent_files.py
@@ -26,29 +26,71 @@ class RecentFiles:
         self.head = None
 
     def is_full(self):
-        return self.load == self.capacity
+        if self.load == self.capacity:
+            return True
+        else:
+            return False
 
     def searchForFileByPath(self, path):
         current = self.head
         while current:
             if current.path == path:
                 return current
+            current = current.next
         return None
 
+    def append_head(self, opened_file):
+        opened_file.next = self.head
+        self.head = opened_file
+        return
+
+    def print_list(self):
+        current = self.head
+        while current is not None:
+            current = current.next
+        return
+
+    def remove_and_return_file_in_the_middle(self, opened_file):
+
+        current = self.head
+        target = None
+
+        if current.path == opened_file.path:
+            return current
+
+        while current.next.path != opened_file.path:
+            current = current.next
+        target = current.next
+        current.next = target.next
+        return target
+
+    def remove_oldest_file(self):
+        fast = self.head.next
+        slow = self.head
+
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = None
+        fast = None
+        return
+
     def register_file(self, opened_file):
+
+        # If storage is full, remove oldest item and add the new one
+        if self.is_full() is True:
+            self.remove_oldest_file()
+            self.append_head(opened_file)
+            return
+
+        # Check wether the file is already known by the RecentFiles
         file_exists = self.searchForFileByPath(opened_file.path)
-        if file_exists is not None:
-            opened_file.next = self.head
-            self.head = opened_file
+
+        if file_exists:
+            known_file = self.remove_and_return_file_in_the_middle(file_exists)
+            self.append_head(known_file)
+            return
         else:
-            current = self.head
-
-            if current is None:
-                self.head = opened_file
-            else:
-                while current.next:
-                    current = current.next
-                current.next = opened_file
-
+            self.append_head(opened_file)
             self.load += 1
             return
--- tests/recent_files/test_recent_files.py
+++ tests/recent_files/test_recent_files.py
@@ -5,6 +5,7 @@ sys.path.insert(
 
 from recent_files import RecentFiles, File
 
+
 # - Quando o programa está executando pela primeira vez, essa lista está vazia;
 # - Quando um arquivo é aberto, ele é adicionado à lista de arquivos recentes;
 # - Se um arquivo aberto já está na lista, este é levado ao topo da lista, sem conter itens duplicados na lista
@@ -25,8 +26,9 @@ def test_recent_files_initialization_forbidden_capacity():
     with pytest.raises(ValueError):
         recent_files = RecentFiles(-1)
 
-
 # Opening files
+
+
 def test_recent_files_register_new_file_success():
     recent_files = RecentFiles(10)
     opened_file = File("file1.txt", "This is file one")
@@ -34,6 +36,63 @@ def test_recent_files_register_new_file_success():
     assert recent_files.load == 1
     assert recent_files.searchForFileByPath(
         opened_file.path).content == opened_file.content
+
+
+def test_recent_files_register_multiple_files_success():
+    recent_files = RecentFiles(10)
+    opened_file = File("file1.txt", "This is file 1")
+    opened_file2 = File("file2.txt", "This is file 2")
+    recent_files.register_file(opened_file)
+    recent_files.register_file(opened_file2)
+    assert recent_files.load == 2
+    assert recent_files.searchForFileByPath(
+        opened_file.path).content == opened_file.content
+    assert recent_files.searchForFileByPath(
+        opened_file2.path).content == opened_file2.content
+
+
+def test_recent_files_register_known_file_success():
+    recent_files = RecentFiles(10)
+    opened_file = File("file1.txt", "This is file 1")
+    opened_file2 = File("file2.txt", "This is file 2")
+    opened_file3 = File("file1.txt", "This is file 1")
+    recent_files.register_file(opened_file)
+    recent_files.register_file(opened_file2)
+    recent_files.print_list()
+    recent_files.register_file(opened_file3)
+    assert recent_files.load == 2
+    assert recent_files.searchForFileByPath(
+        opened_file.path).content == opened_file.content
+    assert recent_files.searchForFileByPath(
+        opened_file2.path).content == opened_file2.content
+    assert recent_files.searchForFileByPath(
+        opened_file3.path).content == opened_file3.content
+
+    # Make sure the known_file is the first element in the list
+    assert recent_files.head.path == opened_file3.path
+
+
+def test_recent_files_register_over_limit():
+    recent_files = RecentFiles(3)
+    opened_file = File("file1.txt", "This is file 1")
+    opened_file2 = File("file2.txt", "This is file 2")
+    opened_file3 = File("file3.txt", "This is file 3")
+    opened_file4 = File("file4.txt", "This is file 4")
+    recent_files.register_file(opened_file)
+    recent_files.register_file(opened_file2)
+    recent_files.register_file(opened_file3)
+    recent_files.register_file(opened_file4)
+    assert recent_files.load == 3
+
+    # Make sure the oldest file gets removed
+    assert recent_files.searchForFileByPath(
+        opened_file.path) is None
+    assert recent_files.searchForFileByPath(
+        opened_file2.path).content == opened_file2.content
+    assert recent_files.searchForFileByPath(
+        opened_file3.path).content == opened_file3.content
+    assert recent_files.searchForFileByPath(
+        opened_file4.path).content == opened_file4.content
 
 # Flushing Recent Files
 
